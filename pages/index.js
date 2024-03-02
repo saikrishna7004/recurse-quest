371612ea49213085a2d305c9664fb6d3831344ba
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2024-03-02 13:48:06 +0530

    Removed timer

--- pages/index.js
+++ pages/index.js
@@ -4,33 +4,33 @@ import Head from 'next/head';
 
 const HomePage = () => {
     // Set your target date and time for the treasure hunt
-    const targetDate = new Date('2024-03-02T12:00:00').getTime();
-    const [countdown, setCountdown] = useState('');
-    const [startButtonDisabled, setStartButtonDisabled] = useState(true); // Initialize as disabled
+    // const targetDate = new Date('2024-03-02T12:00:00').getTime();
+    // const [countdown, setCountdown] = useState('');
+    // const [startButtonDisabled, setStartButtonDisabled] = useState(true); // Initialize as disabled
 
-    useEffect(() => {
-        const updateCountdown = () => {
-            const now = new Date().getTime();
-            const distance = targetDate - now;
+    // useEffect(() => {
+    //     const updateCountdown = () => {
+    //         const now = new Date().getTime();
+    //         const distance = targetDate - now;
 
-            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    //         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    //         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    //         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    //         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-            const countdownString = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    //         const countdownString = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
-            setCountdown(countdownString);
+    //         setCountdown(countdownString);
 
-            if (distance <= 0) {
-                setStartButtonDisabled(false);
-            }
-        };
+    //         if (distance <= 0) {
+    //             setStartButtonDisabled(false);
+    //         }
+    //     };
 
-        const countdownInterval = setInterval(updateCountdown, 1000);
+    //     const countdownInterval = setInterval(updateCountdown, 1000);
 
-        return () => clearInterval(countdownInterval);
-    }, [targetDate]);
+    //     return () => clearInterval(countdownInterval);
+    // }, [targetDate]);
 
     return (<>
             <Head>
@@ -39,7 +39,7 @@ const HomePage = () => {
             <div className='container text-center' style={{ height: '90vh', display: 'flex', textAlign: 'center', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
                 <img src="/coinquest logo.png" alt="Recurse Logo White" style={{width: '100%', marginTop: '50px', marginBottom: '30px'}} />
                 <h3 style={{marginBottom: '10px', marginTop: '10px'}}>Are you excited for the Quest to begin?</h3>
-                <h3 style={{marginTop: '10px', marginBottom: '10px', padding: '10px'}}>The quest will start in: <br /><p style={{fontSize: '60px', margin: '10px 0 0 0', fontWeight: '600', fontFamily: 'sans-serif'}}>{countdown}</p></h3>
+                <h3 style={{marginTop: '10px', marginBottom: '10px', padding: '10px'}}>The quest has started</h3>
                 <div className='buttons'>
                     <Link className={`button button1`} target='_blank' href="https://drive.google.com/file/d/1wsrBxEw2klM5bggsGhiyBHwaa3bAn3E-">Event Details</Link>
                     <Link className={`button`} target='_blank' href="https://recurse-coinquest.vercel.app/">Register/Login</Link>
